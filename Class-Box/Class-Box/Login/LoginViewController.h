1a57112ea6b6d8742a4276896a50799e452b619e
sherlock <251879836@qq.com>
2017-10-14 09:44:12 +0800

    update

---
+++ Class-Box/Class-Box/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Class-Box
+//
+//  Created by sherlock on 2017/10/13.
+//  Copyright © 2017年 sherlock. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LoginViewController : BaseViewController
+
+@end
---
+++ Class-Box/Class-Box/Login/SchoolLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SchoolLoginViewController.h
+//  Class-Box
+//
+//  Created by sherlock on 2017/10/13.
+//  Copyright © 2017年 sherlock. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SchoolLoginViewController : BaseViewController
+
+@end
